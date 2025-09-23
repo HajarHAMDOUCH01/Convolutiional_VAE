e4d8367ae235692919d687a8e5d4673342a7cc1f
HajarHAMDOUCH01 <hajar.hamdouch02@gmail.com>
2025-09-23 19:12:49 +0100

    2 faces mix test

--- test.py
+++ test.py
@@ -16,8 +16,11 @@ z_dim = 256  # Use whatever z_dim you trained with
 model = ConvolutionnalVAE(z_dim=z_dim, input_size=256)  # Adjust based on your training
 model.load_state_dict(checkpoint['model_state_dict'])
 model.eval()  
-image_face1 = 
-image_face2 = 
+image_face1 = "/content/face1.jpg"
+image_face2 = "/content/test_face.jpg"
+
+face1 = Image.open(image_face1).convert("RGB")
+face2 = Image.open(image_face2).convert("RGB")
 
 def generate_new_face(z_dim, model):
     with torch.no_grad():
@@ -37,23 +40,32 @@ def generate_new_face(z_dim, model):
 def interpolate_2_faces(model, face1, face2, alpha=0.5):
     with torch.no_grad():
         transform = transforms.Compose([
-            transforms.Resize((256*256)),
+            transforms.Resize((256, 256)),
             transforms.ToTensor()
         ])
-        face1_tensor = transform(face1)
-        face2_tensor = transform(face2)
-        z1 = model.encode(face1_tensor)
-        z2 = model.encode(face2_tensor)
 
-        z_seed = z1 * alpha + z2 * (1-alpha)
+        face1_tensor = transform(face1).unsqueeze(0)  # [1, 3, 256, 256]
+        face2_tensor = transform(face2).unsqueeze(0)
 
+        # Encode
+        _, mu1 , logvar1 = model.encode(face1_tensor)
+        _, mu2 , logvar2 = model.encode(face2_tensor)
+
+        z1 = model.reparametrize(mu1, logvar1)
+        z2 = model.reparametrize(mu2, logvar2)
+
+        # Interpolate
+        z_seed = z1 * (1 - alpha) + z2 * alpha
+        z_seed = z_seed.unsqueeze(0) if z_seed.ndim == 1 else z_seed
+
+        # Decode
         mixed_face = model.decode(z_seed)
 
+    # Convert to PIL
     to_pil = ToPILImage()
-    image = to_pil(mixed_face.squeeze(0))  
-
-    # Save with PIL
-    image.save('mixed_face.png')
+    image = to_pil(mixed_face.squeeze(0).cpu().clamp(0,1))
+    image.save("mixed_face.png")
+    return image
 
 if __name__=="__main__":
-    interpolate_2_faces(model, image_face1, image_face2, alpha=0.5)
+    interpolate_2_faces(model, face1, face2, alpha=0.5)
